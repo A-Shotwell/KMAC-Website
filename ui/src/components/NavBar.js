3284d07ceb2e01c3e4b0ccb52dbf39e8ea5dc09e
Aaron Shotwell <a.shotwell.websolutions@gmail.com>
2022-07-10 12:13:18 -0700

    Navbar CSS complete

--- ui/src/components/NavBar.js
+++ ui/src/components/NavBar.js
@@ -1,12 +1,20 @@
+/*
+
+  PROBLEMS:
+    - clicking the dropdown menu button and then resizing the window back to desktop width leaves dropdown open.
+
+*/
+
 import { React, useEffect, useState } from "react";
 import styles from "./NavBarCSS.module.css";
 
 const NavBar = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [dropMenu, setDropMenu] = useState(false);
     
     const handleScroll = () => {
-    const position = window.pageYOffset;
-    setScrollPosition(position);
+      const position = window.pageYOffset;
+      setScrollPosition(position);
     };
 
     useEffect(() => {
@@ -17,33 +25,62 @@ const NavBar = () => {
         };
     }, []);
 
+    const handleMenuClick = () => {
+      setDropMenu(prev => !prev)
+    }
+
+    const dropDown = {
+      
+    }
+
    return (
     <div className={styles.main}>
       <nav className={styles.container}>
         <div className={`${styles.nav} ${styles.navLeft}`}>
           <ul>
             <li>
-              <a href="">HOME</a>
+              <button role="button" onClick={null}>HOME</button>
             </li>
             <li>
-              <a href="">ABOUT</a>
+              <button role="button" onClick={null}>ABOUT</button>
             </li>
           </ul>
         </div>
         <div className={`${styles.nav} ${styles.navRight}`}>
           <ul>
             <li>
-              <a href="">SHOWS</a>
+              <button role="button" onClick={null}>SHOWS</button>
             </li>
             <li>
-              <a href="">CONTACT</a>
+              <button role="button" onClick={null}>CONTACT</button>
             </li>
           </ul>
         </div>
       </nav>
-      {/* <img src="images/logo.png" alt="KMAC logo" className={`${styles.logo} ${scroll ? styles.logoNav : styles.logoHero}`} /> */}
+      {
+          dropMenu
+          ? <img src="images/CloseMenuButton.png" className={styles.menuButton} alternative="Menu" onClick={handleMenuClick} />
+          : <img src="images/MenuButton.png" className={styles.menuButton} alternative="Menu" onClick={handleMenuClick} />
+      }
+      { dropMenu && 
+        <nav className={styles.navDrop}>
+          <ul>
+            <li>
+              <button role="button" onClick={null}>HOME</button>
+            </li>
+            <li>
+              <button role="button" onClick={null}>ABOUT</button>
+            </li>
+            <li>
+              <button role="button" onClick={null}>SHOWS</button>
+            </li>
+            <li>
+              <button role="button" onClick={null}>CONTACT</button>
+            </li>
+          </ul>
+        </nav>
+      }
       <img src="images/logo.png" alt="KMAC logo" className={`${styles.logo} ${scrollPosition > 0 ? styles.logoNav : styles.logoHero}`} />
-      {/* <button style={{color: "red"}} onClick={() => setScroll(!scroll)}>TOGGLE LOGO POSITION</button> */}
     </div>
   );
 };
