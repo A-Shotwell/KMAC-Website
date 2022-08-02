d37a9a7011391af6916d8acc0ddc5362dd1555d4
Aaron Shotwell <a.shotwell.websolutions@gmail.com>
2022-08-02 13:29:55 -0700

    Progress on form validation. I feel like there is too much repeated code here. I shouldn't have to check validation twice for invalid alerts and submission. I need to find a way to simplify this. I also need to include a phone number check for non-number characters.

--- ui/src/components/ContactAndBooking.js
+++ ui/src/components/ContactAndBooking.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import styles from "./ContactAndBooking.module.css"
 
 const ContactAndBooking = () => {
+    // Contact form values
     const [contact, setContact] = useState({
         name: null,
         email: null,
@@ -9,6 +10,8 @@ const ContactAndBooking = () => {
         reason: null,
         message: null
     })
+
+    // Booking form values
     const [booking, setBooking] = useState({
         name: null,
         email: null,
@@ -17,10 +20,17 @@ const ContactAndBooking = () => {
         time: null,
         description: null
     })
-    const [minDate, setMinDate] = useState()
-    const [typeContact, setTypeContact] = useState("booking")
-    // const [valid, setValid] = useState(true)
 
+    // Store today's date, calendar selection minimum
+    const [minDate, setMinDate] = useState()
+
+    // Display contact form or booking form, switch on button press
+    const [typeContact, setTypeContact] = useState("booking")
+
+    // Flag invalid form values for validation alerts
+    const [validFlag, setValidFlag] = useState(true)
+
+    // Keep track of today's date, booking calendar selection minimum
     useEffect(()=> {
         let today = new Date
         let dd = today.getDate()
@@ -36,52 +46,71 @@ const ContactAndBooking = () => {
         setMinDate(today)
     })
 
+    // Validate form values
     const validate = () => {
+        // Track and return form validity, switch to false if any field fails
         let isValid = true
 
+        // Check validity of contact form values
         if (typeContact === "contact"){
+            // If no name, trigger invalid flag
             if (!contact.name){
                 isValid = false
             }
+            // If no email or email does not match regex, trigger invalid flag
             if (!contact.email || !contact.email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
                 isValid = false
             }
-            if (![10,11].includes(contact.phone.split('-').join('').split('').length)){
+            // If phone number length is neither 10 nor 11 digits long, trigger invalid flag
+            if (!contact.phone || ![10,11].includes(contact.phone.split('-').join('').split('').length)){
                 isValid = false
             }
+            // If no reason for contact is given, trigger invalid flag
             if (!contact.reason){
                 isValid = false
             }
+            // If message field is blank, trigger invalid flag
             if (!contact.message){
                 isValid = false
             }
         }
+        // Check validity of booking form values
         else if (typeContact === "booking"){
+            // If no name, trigger invalid flag
             if (!booking.name){
                 isValid = false
             }
+            // If no email or email does not match regex, trigger invalid flag
             if (!booking.email || !booking.email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
                 isValid = false
             }
-            if (![10,11].includes(booking.phone.split('-').join('').split('').length)){
+            // If phone number length is neither 10 nor 11 digits long, trigger invalid flag
+            if (!booking.phone || ![10,11].includes(booking.phone.split('-').join('').split('').length)){
                 isValid = false
             }
+            // If no booking date is selected, trigger invalid flag
             if (!booking.date){
                 isValid = false
             }
+            // If no booking time is selected, trigger invalid flag
             if (!booking.time){
                 isValid = false
             }
+            // If no booking description is given, trigger invalid flag
             if (!booking.description){
                 isValid = false
             }
         }
+
+        // set form validation alert flag on validation failure
+        !isValid ? setValidFlag(false) : setValidFlag(true)
 
         return isValid
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        setValidFlag(true)
         if (validate()){
             if (typeContact === "contact"){
                 console.log(contact)
@@ -90,11 +119,14 @@ const ContactAndBooking = () => {
                 console.log(booking)
             }
         }
+        // set form validation alert flag on validation failure
         else {
+            setValidFlag(false)
             console.log("INVALID")
         }
     }
 
+    // Contact form view
     const contactWindow = () => {
         return (
             <form onSubmit={e => handleSubmit(e)}>
@@ -105,6 +137,7 @@ const ContactAndBooking = () => {
                     type="text" 
                     onChange={e => setContact({...contact, name: e.target.value})} 
                 />
+                {!validFlag && !contact.name && <span>Please enter your name.</span>}
                 <label className={styles.label} htmlFor="email">Email: </label>
                 <input 
                     className={styles.input} 
@@ -112,6 +145,7 @@ const ContactAndBooking = () => {
                     type="email" 
                     onChange={e => setContact({...contact, email: e.target.value})} 
                 />
+                {!validFlag && (!contact.email || !contact.email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) && <span>Please enter a valid email address.</span>}
                 <label className={styles.label} htmlFor="phone">Phone: </label>
                 <input 
                     className={styles.input} 
@@ -119,6 +153,7 @@ const ContactAndBooking = () => {
                     type="text" 
                     onChange={e => setContact({...contact, phone: e.target.value})} 
                 />
+                {!validFlag && (!contact.phone || ![10,11].includes(contact.phone.split('-').join('').split('').length)) && <span>Please enter a valid phone number.</span>}
                 <select 
                     className={styles.select} 
                     onChange={e => setContact({...contact, reason: e.target.value})}
@@ -128,16 +163,19 @@ const ContactAndBooking = () => {
                     <option className={styles.option} value="Business Inquiry">Business Inquiry</option>
                     <option className={styles.option} value="Other">Other</option>
                 </select>
+                {!validFlag && !contact.reason && <span>Please select a reason for contact.</span>}
                 <textarea 
                     className={styles.textarea} 
                     placeholder="How can I help you?" 
                     onChange={e => setContact({...contact, message: e.target.value})} 
                 />
+                {!validFlag && !contact.message && <span>Please enter your message.</span>}
                 <button role="submit">Submit</button>
             </form>
         )
     }
 
+    // Booking form view
     const bookingWindow = () => {
         return (
             <form onSubmit={handleSubmit}>
@@ -148,6 +186,7 @@ const ContactAndBooking = () => {
                     type="text" 
                     onChange={e => setBooking({...booking, name: e.target.value})} 
                 />
+                {!validFlag && !booking.name && <span>Please enter your name.</span>}
                 <label className={styles.label} htmlFor="email">Email: </label>
                 <input 
                     className={styles.input} 
@@ -155,6 +194,7 @@ const ContactAndBooking = () => {
                     type="email" 
                     onChange={e => setBooking({...booking, email: e.target.value})} 
                 />
+                {!validFlag && (!booking.email || !booking.email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) && <span>Please enter a valid email address.</span>}
                 <label className={styles.label} htmlFor="phone">Phone: </label>
                 <input 
                     className={styles.input} 
@@ -162,6 +202,7 @@ const ContactAndBooking = () => {
                     type="text" 
                     onChange={e => setBooking({...booking, phone: e.target.value})} 
                 />
+                {!validFlag && (!booking.phone || ![10,11].includes(booking.phone.split('-').join('').split('').length)) && <span>Please enter a valid phone number.</span>}
                 <label className={styles.label} htmlFor="date">Select Date: </label>
                 <input 
                     type="date" 
@@ -169,17 +210,20 @@ const ContactAndBooking = () => {
                     min={minDate}
                     onChange={e => setBooking({...booking, date: e.target.value})} 
                 />
+                {!validFlag && !booking.date && <span>Please select a date for your event.</span>}
                 <label className={styles.label} htmlFor="time">Select Time: </label>
                 <input 
                     type="time"
                     name="time"
                     onChange={e => setBooking({...booking, time: e.target.value})}
                 />
+                {!validFlag && !booking.time && <span>Please select a time for your event.</span>}
                 <textarea 
                     className={styles.textarea} 
-                    placeholder="Please describe your event." 
+                    placeholder="Please give a brief description of your event." 
                     onChange={e => setBooking({...booking, description: e.target.value})} 
                 />
+                {!validFlag && !booking.description && <span>Please describe your event.</span>}
                 <button role="submit">Submit</button>
             </form>
         )
@@ -187,8 +231,24 @@ const ContactAndBooking = () => {
 
     return (
         <div>
-            <button role="button" onClick={() => setTypeContact("contact")}>Contact</button>
-            <button role="button" onClick={() => setTypeContact("booking")}>Booking</button>
+            <button 
+                role="button" 
+                onClick={() => {
+                    setTypeContact("contact")
+                    setValidFlag(true)
+                }}
+            >
+                Contact
+            </button>
+            <button 
+                role="button" 
+                onClick={() => {
+                    setTypeContact("booking")
+                    setValidFlag(true)
+                }}
+            >
+                Booking
+            </button>
             <br />
             {typeContact === "contact" && contactWindow()}
             {typeContact === "booking" && bookingWindow()}
