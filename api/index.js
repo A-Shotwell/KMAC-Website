2af30a1bdacb65631392d2340623323722c3e52c
Aaron Shotwell <a.shotwell.websolutions@gmail.com>
2022-09-12 19:26:47 -0700

    Working on contact/booking email functions in the API. Contact works, but only with environmental variable set to ignore untrusted certificates as a work around. Need to find a better solution.

--- api/index.js
+++ api/index.js
@@ -1,28 +1,52 @@
 import mongoose from 'mongoose'
 import express from 'express'
 import * as dotenv from 'dotenv'
+import nodemailer from 'nodemailer'
 
 dotenv.config()
 
 const app = express()
+const port = process.env.PORT
+app.listen(port, () => console.log(`Server listening on port ${port}`))
 
 async function main() {
     await mongoose.connect(process.env.MONGOOSE_CONNECT)
 }
+// main.catch(err => console.log(err))
 
-app.get('/getShow', (req, res) => {
+// EMAIL SERVICE TRANSPORTER
+const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS
+    }
+})
+
+app.get('/getShow', function(req, res){
     res.send('GET SHOW')
 })
 
-app.post('/postShow', (req, res) => {
+app.post('/postShow', function(req, res){
     res.send('POST SHOW')
 })
 
-app.post('contact', (req, res) => {
-    res.send('CONTACT')
+app.get('/contact', function(req, res){
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: 'aaronspleasantnightmares@gmail.com',
+        subject: 'TEST CONTACT',
+        text: 'TEST CONTACT SUCCESSFUL'
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+        error ? console.log(error) : console.log('Email sent: ' + info.response)
+    })
+
+    res.send('CONTACT SENT')
 })
 
-app.post('booking', (req, res) => {
+app.post('booking', function(req, res){
     res.send('BOOKING')
 })
 
